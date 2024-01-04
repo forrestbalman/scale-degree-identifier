f893717eaff945a940aebb9602abc9c5699e175b
forrestbalman <ifork1@gmail.com>
2024-01-03 16:57:26 -0800

    rebuilt attempt to fix sound

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.MAAftAaz.js","_app/immutable/chunks/scheduler.k-kUyWhY.js","_app/immutable/chunks/index.TEwWPNSA.js","_app/immutable/chunks/singletons.5abTiN57.js"];
+export const imports = ["_app/immutable/nodes/1.r8MxFKru.js","_app/immutable/chunks/scheduler.k-kUyWhY.js","_app/immutable/chunks/index.TEwWPNSA.js","_app/immutable/chunks/singletons.U2un4Fdf.js"];
 export const stylesheets = [];
 export const fonts = [];
